be46d3593ffda63f96a63cdce7b7556970f7e1c3
jjcc0410 <jjcc0410>
2024-11-17 18:28:29 +0000

    added gold layer

--- Untitled Notebook 2024-11-17 12_27_33.py
+++ Untitled Notebook 2024-11-17 12_27_33.py
@@ -37,4 +37,40 @@ class Bronze():
 
 # COMMAND ----------
 
+class Gold():
+    def __init__(self):
+        self.base_data_dir = "/FileStore/data_spark_streaming_scholarnest"
+        
+    def readBronze(self):
+        return spark.readStream.table("invoices_bz")
 
+    def getAggregates(self, invoices_df):
+        from pyspark.sql.functions import sum, expr
+        return (invoices_df.groupBy("CustomerCardNo")
+                    .agg(sum("TotalAmount").alias("TotalAmount"),
+                         sum(expr("TotalAmount*0.02")).alias("TotalPoints"))
+                )
+
+    def upsert(self, rewards_df, batch_id):
+        rewards_df.createOrReplaceTempView("customer_rewards_df_temp_view")
+        merge_statement = """MERGE INTO customer_rewards t
+                USING customer_rewards_df_temp_view s
+                ON s.CustomerCardNo == t.CustomerCardNo
+                WHEN MATCHED THEN
+                UPDATE SET t.TotalAmount = s.TotalAmount, t.TotalPoints = s.TotalPoints
+                WHEN NOT MATCHED THEN
+                INSERT *
+            """
+        rewards_df._jdf.sparkSession().sql(merge_statement)
+
+    def process(self):
+        invoices_df = self.readBronze()
+        aggregate_df = self.getAggregates(invoices_df)
+        sQuery = (aggregate_df.writeStream
+                        .queryName("gold-update")
+                        .option("checkpointLocation", f"{self.base_data_dir}/chekpoint/customer_rewards")
+                        .outputMode("update")
+                        .foreachBatch(self.upsert)
+                        .start()
+                  )
+        return sQuery
